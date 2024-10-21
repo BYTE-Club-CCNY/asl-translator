c4a78e9f2c88dd495c7f42c5ae9cf580d9164330
Aaron J <jamesacs329@gmail.com>
2024-10-21 15:09:18 -0400

    style: clean comments

--- handlandmarker.py
+++ handlandmarker.py
@@ -17,13 +17,12 @@ if __name__ == "__main__":
     options = HandLandmarkerOptions(
         base_options=BaseOptions(model_asset_path=os.getcwd()+model_path),
         running_mode=VisionRunningMode.IMAGE,
-        num_hands = 2 # i think some asl needs two hands?
+        num_hands = 2 
     )
     with HandLandmarker.create_from_options(options) as landmarker:
         result = landmarker.detect(mp_image)
         print(result)
         with open(datetime.datetime.now().strftime("%m-%d-%Y-%H-%M-%S.txt"),"a") as f:
-            # prompt: write the results to a text file
             for idx in range(len(result.hand_landmarks)):
                 hand_landmarks = result.hand_landmarks[idx]
                 handedness = result.handedness[idx]
