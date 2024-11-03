16d177401744b21ee18b3c9768ad42b7568bd3a8
Aaron J <jamesacs329@gmail.com>
2024-11-03 18:38:12 -0500

    feat: output json files

--- handlandmarker.py
+++ handlandmarker.py
@@ -3,7 +3,8 @@ import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 import os 
-
+from dataclasses import asdict
+import json
 BaseOptions = mp.tasks.BaseOptions
 HandLandmarker = mp.tasks.vision.HandLandmarker
 HandLandmarkerOptions = mp.tasks.vision.HandLandmarkerOptions
@@ -12,7 +13,7 @@ VisionRunningMode = mp.tasks.vision.RunningMode
 model_path = '/hand_landmarker.task'
     
 if __name__ == "__main__":
-    img = mp_image = mp.Image.create_from_file(os.getcwd()+'/test/a.jpg')
+    img = mp_image = mp.Image.create_from_file(os.getcwd()+'/test/twohands.jpg')
 
     options = HandLandmarkerOptions(
         base_options=BaseOptions(model_asset_path=os.getcwd()+model_path),
@@ -21,15 +22,10 @@ if __name__ == "__main__":
     )
     with HandLandmarker.create_from_options(options) as landmarker:
         result = landmarker.detect(mp_image)
-        print(result)
-        with open(datetime.datetime.now().strftime("%m-%d-%Y-%H-%M-%S.txt"),"a") as f:
-            for idx in range(len(result.hand_landmarks)):
-                hand_landmarks = result.hand_landmarks[idx]
-                handedness = result.handedness[idx]
-                f.write(f"Hand {idx}:\n")
-                f.write(f"Handedness: {handedness[0].category_name}\n")
-                for landmark_idx, landmark in enumerate(hand_landmarks):
-                    f.write(f"Landmark {landmark_idx}: x={landmark.x:.4f}, y={landmark.y:.4f}, z={landmark.z:.4f}\n")
-                f.write("\n")
+        # print(result)
+        with open(datetime.datetime.now().strftime("%m-%d-%Y-%H-%M-%S.json"),"a") as f:
+            f.write(json.dumps(asdict(result)))
+                
+
             
         
